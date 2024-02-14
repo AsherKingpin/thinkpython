8af64b103528e192e5ae3f18e2cdf904186afd7b
AsherKingpin <120887356+AsherKingpin@users.noreply.github.com>
2024-02-14 08:07:12 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,16 @@
+import turtle
+bob = turtle.Turtle()
+def square(t,length):
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+def polygon(t,length,n):
+    for i in range(n):
+        t.fd(length)
+        t.lt(360/n)
+
+
+polygon(bob,100,7)
+
+turtle.mainloop()
